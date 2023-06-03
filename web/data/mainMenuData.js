869fbed54e0d02b92583fc91aece1d52177b1bb6
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2023-06-03 13:48:16 +0530

    Minimal changed of rendering the dashboard localhost:3000

--- web/data/mainMenuData.js
+++ web/data/mainMenuData.js
@@ -95,7 +95,7 @@ export const pageItems = [
 export const dashboardItems = [
   {
     name: "Dashboard",
-    routePath: "/vendor-dashboard",
+    routePath: "/dashboard",
   },
   {
     name: "Booking History",  
