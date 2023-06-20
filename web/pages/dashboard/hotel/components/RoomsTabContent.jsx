c4e8685cc8a4bbf8d68c6497d977a2942c332d5c
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2023-06-20 20:20:53 +0530

    Confilict Solved

--- web/pages/dashboard/hotel/components/RoomsTabContent.jsx
+++ web/pages/dashboard/hotel/components/RoomsTabContent.jsx
@@ -12,121 +12,7 @@ import {getAllRooms, getSelectedRoom, saveRoom} from "../../../../slices/roomSli
 import {getOneHotelRooms} from "../../../../slices/hotelSlice";
 
 const RoomsTabcontent = () => {
-<<<<<<< HEAD
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const hotelId = router.query.id;
 
-  const selectedHotel = useSelector((state) => state.hotel.selectedHotel);
-  const AllRooms = useSelector((state) => state.room.rooms);
-  const selectedHotelRooms = useSelector(state => state.hotel.selectedHotelRooms);
-
-  console.log(selectedHotelRooms);
-
-  useEffect(()=>{
-    dispatch(getOneHotelRooms(hotelId));
-  },[])
-
-
-  const [images, setImages] = useState([]);
-  const [newAmentities, setNewAmentities] = useState({
-    possible:"",
-    numOfBeds: 0,
-    amountPerNight:""
-  });
-
-  const [roomAddon, setRoomAddon] = useState({
-    addonId :"",
-    amount:""
-  })
-  // const [extraBed, setExtraBed] = useState(0);
-  const [hotelRoom, setHotelRoom] = useState({
-    hotelId: "",
-    name: "",
-    benefits: "",
-    roomPrice: "",
-    roomArea: "",
-    adults: "",
-    children: "",
-    facilities: [],
-    addons: [],
-    amenities: [],
-  });
-
-  const onChange = (id, value) => {
-    const newHotelRoom = { ...hotelRoom, [id]: value };
-    setHotelRoom(newHotelRoom);
-  };
-  const onRoomSave = () => {
-    let formData = new FormData();
-
-    formData.append("hotelId", hotelId);
-    formData.append("name", hotelRoom.name);
-    formData.append("benefits", hotelRoom.benefits);
-    formData.append("roomPrice", hotelRoom.roomPrice);
-    formData.append("roomArea", hotelRoom.roomArea);
-    formData.append("roomImages", images);
-    formData.append("numOfAdults", hotelRoom.adults);
-    formData.append("numOfChild", hotelRoom.children);
-    formData.append("facilities", JSON.stringify(hotelRoom));
-    formData.append("addons", JSON.stringify(roomAddon));
-    formData.append("amenities", JSON.stringify(newAmentities));
-
-    console.log(hotelRoom);
-
-    // dispatch(saveRoom(formData))
-    //   .unwrap()
-    //   .then((res) => {
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  };
-
-  const onDelete =(id)=>{
-    
-  }
-
-  return (
-    <>
-      <div className="row x-gap-20 y-gap-20">
-        <div className="col-12">
-          <div className="text-18 fw-500 mb-10">Available Rooms</div>
-          <div className="form-input col-10">
-            <input
-              type="text"
-              required
-              id="name"
-              // defaultValue={(item && hotelId && item.hotel) ? item.name : ''}
-              onChange={(evt) => onChange(evt.target.id, evt.target.value)}
-            />
-            <label className="lh-1 text-16 text-light-1">Room Type Name</label>
-          </div>
-
-          {/* End BannerUploader */}
-
-          <div className="mt-30">
-            <div className="fw-500">Room Image</div>
-            <RoomImgUploader
-              hotelRoom={hotelRoom}
-              setHotelRoom={setHotelRoom}
-              images={images}
-              setImages={setImages}
-              hotelId={hotelId}
-            />
-          </div>
-          {/* End RoomImageUploader */}
-
-          <div className="mt-30">
-            <div className="fw-500 mb-10 text-18">Room Sleep Size</div>
-            <div className="accordion -simple row y-gap-20 js-accordion">
-              {/* <Faq /> */}
-              <RoomSleepSize
-                hotelRoom={hotelRoom}
-                setHotelRoom={setHotelRoom}
-                hotelId={hotelId}
-              />
-=======
     const dispatch = useDispatch();
     const router = useRouter();
     const hotelId = router.query.id;
@@ -343,7 +229,6 @@ const RoomsTabcontent = () => {
                         <RoomDetails rooms={selectedHotelRooms} onEdit={onEdit}/>
                     </div>
                 </div>
->>>>>>> 5261033f33b4b369cbd77a70e05da2e3c144c46b
             </div>
         </>
     );
