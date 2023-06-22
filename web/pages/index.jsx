745a42acb0f7de74725ff12868993fb2e4b4365b
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2023-06-22 14:28:58 +0530

    Fixing home page issues

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,19 +1,16 @@
-import dynamic from "next/dynamic";
-import AddBanner from "../../components/add-banner/AddBanner";
-import Seo from "../../components/common/Seo";
-import PopularDestinations from "../../components/destinations/PopularDestinations";
-import DefaultFooter from "../../components/footer/default";
-import Header1 from "../../components/header/header-1";
-import Hero1 from "../../components/hero/hero-1";
-import CallToActions from "../../components/common/CallToActions";
-import Hotels from "../../components/hotels/Hotels";
-import {wrapper} from "../../app/store";
-import {getAllPopularSiteHotels} from "../../slices/hotelSlice";
+import AddBanner from "../components/add-banner/AddBanner";
+import Seo from "../components/common/Seo";
+import PopularDestinations from "../components/destinations/PopularDestinations";
+import DefaultFooter from "../components/footer/default";
+import Header1 from "../components/header/header-1";
+import Hero1 from "../components/hero/hero-1";
+import CallToActions from "../components/common/CallToActions";
+import Hotels from "../components/hotels/Hotels";
+import {wrapper} from "../app/store";
+import {getAllPopularSiteHotels} from "../slices/hotelSlice";
 import {useState} from "react";
 
 const index = (props) => {
-    console.log(props)
-
     const [popularHotels, setPopularHotels] = useState(props.popularHotelDetails);
 
     return (
@@ -27,38 +24,38 @@ const index = (props) => {
             <Hero1/>
             {/* End Hero 1 */}
 
-            <section className="layout-pt-lg layout-pb-md" data-aos="fade-up">
-                <div className="container">
-                    <div className="row y-gap-20 justify-between items-end">
-                        <div className="col-auto">
-                            <div className="sectionTitle -md">
-                                <h2 className="sectionTitle__title">Popular Destinations</h2>
-                                <p className=" sectionTitle__text mt-5 sm:mt-0">
-                                    These popular destinations have a lot to offer
-                                </p>
-                            </div>
-                        </div>
-                        {/* End col-auto */}
+            {/*<section className="layout-pt-lg layout-pb-md" data-aos="fade-up">*/}
+            {/*    <div className="container">*/}
+            {/*        <div className="row y-gap-20 justify-between items-end">*/}
+            {/*            <div className="col-auto">*/}
+            {/*                <div className="sectionTitle -md">*/}
+            {/*                    <h2 className="sectionTitle__title">Popular Destinations</h2>*/}
+            {/*                    <p className=" sectionTitle__text mt-5 sm:mt-0">*/}
+            {/*                        These popular destinations have a lot to offer*/}
+            {/*                    </p>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*            /!* End col-auto *!/*/}
 
-                        <div className="col-auto md:d-none">
-                            <a
-                                href="#"
-                                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
-                            >
-                                View All Destinations
-                                <div className="icon-arrow-top-right ml-15"/>
-                            </a>
-                        </div>
-                        {/* End col-auto */}
-                    </div>
-                    {/* End .row */}
+            {/*            <div className="col-auto md:d-none">*/}
+            {/*                <a*/}
+            {/*                    href="#"*/}
+            {/*                    className="button -md -blue-1 bg-blue-1-05 text-blue-1"*/}
+            {/*                >*/}
+            {/*                    View All Destinations*/}
+            {/*                    <div className="icon-arrow-top-right ml-15"/>*/}
+            {/*                </a>*/}
+            {/*            </div>*/}
+            {/*            /!* End col-auto *!/*/}
+            {/*        </div>*/}
+            {/*        /!* End .row *!/*/}
 
-                    <div className="relative pt-40 sm:pt-20">
-                        {/*<PopularDestinations/>*/}
-                    </div>
-                </div>
-                {/* End .container */}
-            </section>
+            {/*        <div className="relative pt-40 sm:pt-20">*/}
+            {/*            <PopularDestinations/>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    /!* End .container *!/*/}
+            {/*</section>*/}
             {/* End Popular Destinations */}
 
             <section className="layout-pt-md layout-pb-md">
