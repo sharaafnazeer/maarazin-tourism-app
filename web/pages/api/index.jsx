7468409025b303ccec8fc5821833a974a72be570
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2023-06-23 09:47:04 +0530

    changing ip

--- web/pages/api/index.jsx
+++ web/pages/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL:"http://localhost:5000/api/v1",
+    baseURL:"http://146.190.177.204:5000/api/v1",
     headers:{"Content-type":"application/json"},
 });
