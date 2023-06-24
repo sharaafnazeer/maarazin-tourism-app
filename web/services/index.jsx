ae380077238914f259041a13e993128a8b3d7f59
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2023-06-24 16:29:03 +0530

    initial changes to localhost:5000

--- web/services/index.jsx
+++ web/services/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    // baseURL: "http://localhost:5000/api/v1",
-    baseURL: "https://api.rexeholidays.com/api/v1",
+    baseURL: "http://localhost:5000/api/v1",
+    // baseURL: "https://api.rexeholidays.com/api/v1",
     headers: {"Content-type": "application/json"},
 });
