bb0b52cf8a0747a8b70587d454b7a95601fd8a2b
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2023-06-27 17:00:52 +0530

    api url changed

--- web/services/index.jsx
+++ web/services/index.jsx
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 console.log(process.env.NEXT_APP_API_URL)
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://api.rexeholidays.com/api/v1",
     headers: {"Content-type": "application/json"},
 });
