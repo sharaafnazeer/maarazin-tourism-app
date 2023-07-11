647fa0d7c5a59ed8b615a2db08824d323d122d91
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2023-07-11 14:23:00 +0530

    Fixing bugs

--- web/components/hotel-single/Overview.jsx
+++ web/components/hotel-single/Overview.jsx
@@ -23,7 +23,7 @@ const Overview = ({ hotelData }) => {
         <div class="accordion-body">
           Placeholder content for this accordion, which is intended to
           demonstrate the <code>.accordion-flush</code> class. This is the first
-          item's accordion body.
+          item&apos;s accordion body.
         </div>
       </div>
     </>
